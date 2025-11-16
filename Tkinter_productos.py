1de8a0c109e35c3bc1862cba3e661df2aff2977b
tamaravinuela3-maker <tamaravinuela3@gmail.com>
2025-11-16 10:46:18 -0300

    Update Tkinter_productos.py

--- Tkinter_productos.py
+++ Tkinter_productos.py
@@ -4,46 +4,60 @@ ventana = tk.Tk()
 ventana.title('Lista de producto')
 ventana.geometry('1000x800')
 ventana.configure(bg="#e6f7ff")
-ingreso_producto = tk.Entry(ventana)
-font=("Arial", 14)
-ingreso_producto.pack()
+ingreso_producto = tk.Entry(
+    ventana,
+    font=("Arial", 14),
+    bg="#ffffff",
+    fg="#2b4e68"
+)
+ingreso_producto.pack(pady=10)
 ingreso_producto.configure(bg="#ffffff", fg="#4a0072")
 def agregar_producto():
  producto = ingreso_producto.get()
  if producto:lista_producto.insert(tk.END, producto)
  ingreso_producto.delete(0, tk.END)
-boton_agregar = tk.Button(ventana, text = 'Agregar producto', command = agregar_producto)
+boton_agregar = tk.Button(
+ ventana, 
+text = 'Agregar producto', 
+command = agregar_producto,
 bg="#a3d9ff",  
-    fg="#003044",
-    font=("Arial", 12, "bold")
-boton_agregar.pack()
+fg="#003044",
+font=("Arial", 12, "bold")
+)
+boton_agregar.pack(pady=5)
 boton_agregar.configure(bg="#4CAF50", fg="white")
 def eliminar_producto():
  seleccion = lista_producto.curselection()
  if seleccion:lista_producto.delete(seleccion)
-boton_eliminar = tk.Button(ventana, text = 'Eliminar producto', command = eliminar_producto)
+boton_eliminar = tk.Button(
+ ventana, 
+text = 'Eliminar producto', 
+command = eliminar_producto)
 bg="#ffb3b3",   
-    fg="#5a0000",
-    font=("Arial", 12, "bold")
-boton_eliminar.pack()
+fg="#5a0000",
+font=("Arial", 12, "bold")
+')'
+boton_eliminar.pack(pady=5)
 boton_eliminar.configure(bg="#E53935", fg="white")
-lista_producto = tk.Listbox(ventana)
+lista_producto = tk.Listbox(
+ventana,
 font=("Arial", 16, "bold"),
-    fg="#2b4e68",
-    bg="#ffffff",
-    width=35,
-    height=8,
-    highlightthickness=3,
-    highlightbackground="#a3d9ff" 
-lista_producto.pack()
+fg="#2b4e68",
+bg="#ffffff",
+width=35,
+height=8,
+highlightthickness=3,
+highlightbackground="#a3d9ff"
+)
+lista_producto.pack(pady=15)
 
 
 barra_menu = tk.Menu(ventana)
 ventana.config(menu=barra_menu)
-menu_principal = tk.Menu(barra_menu)
+menu_principal = tk.Menu(barra_menu,tearoff=0)
 barra_menu.add_cascade(label =
 'BAZAR', menu=menu_principal)
-submenu = tk.Menu(menu_principal)
+submenu = tk.Menu(menu_principal,tearoff=0)
 menu_principal.add_cascade(label =
 'PLATOS', menu=submenu)
 submenu.add_command(label = 'PLATOS PLAYOS')
@@ -51,13 +65,22 @@ submenu.add_command(label = 'PLATOS HONDOS')
 
 
 ventana.title('Tkinter proyecto grupo 5')
-marco = tk.Frame(ventana)
+marco = tk.Frame(ventana,bg="#e6f7ff")
 marco.pack(padx = 10, pady = 10)
 scrollbar = tk.Scrollbar (marco)
 scrollbar.pack(side = tk.RIGHT, fill = tk.Y)
 
-lista = tk.Listbox(marco, yscrollcommand
-= scrollbar.set)
+lista = tk.Listbox(
+ marco, 
+ yscrollcommand= scrollbar.set,
+ font=("Arial", 14),
+    fg="#2b4e68",
+    bg="#ffffff",
+    highlightthickness=3,
+    highlightbackground="#a3d9ff",
+    width=30,
+    height=10
+)
 productos = ["platos", "cuchillos", "tenedores", "cucharas", "tazas", "manteles", "individuales","pocillos", "teteras", "cafeteras", "repasadores", "jarras", "fuentes"]
 for producto in productos:
  lista.insert(tk.END, producto)
@@ -67,8 +90,13 @@ scrollbar.config(command = lista.yview)
 
 
 import time
-reloj = tk.Label(ventana, font =
-('Arial', 10), bg = 'grey', fg = 'white')
+reloj = tk.Label(
+ ventana, 
+ font =('Arial', 12, "bold"),
+    bg='#99c2ff',  
+    fg='white'
+)
+reloj.pack(pady=10)
 def hora():
  tiempo_actual =time.strftime('%H:%M:%S')
  reloj.config(text = tiempo_actual)
@@ -77,6 +105,7 @@ reloj.pack(anchor = 'center')
 hora()
 
 ventana.mainloop()
+
 
 
 
